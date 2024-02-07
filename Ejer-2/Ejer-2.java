2ab2824788cbc01c00878712d816e8d981e7dd6b
LValdes2003 <lvaldesp@myuax.com>
2024-02-07 21:39:49 +0100

    Ejer-2

---
+++ Ejer-2/Ejer-2.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Mayor número entero representable por un char
+        char mayorChar = Character.MAX_VALUE;
+        System.out.println("Mayor número entero representable por un char: " + (int) mayorChar);
+
+        // Mayor número entero representable por un short
+        short mayorShort = Short.MAX_VALUE;
+        System.out.println("Mayor número entero representable por un short: " + mayorShort);
+
+        // Mayor número entero representable por un int
+        int mayorInt = Integer.MAX_VALUE;
+        System.out.println("Mayor número entero representable por un int: " + mayorInt);
+    }
+}
